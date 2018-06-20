f2e22e404d6554210fe166b23e5ac9af8df2fcfd
Sean Hamilton <sean@hamistudios.com>
2018-06-20 14:15:57 +0100

    Added new colors

--- examples/colors.js
+++ examples/colors.js
@@ -2,7 +2,59 @@ const
   { Colors }  = require('./../index')
 ;
 
-console.log('I am red and underlined'.red.underline);
-console.log('I am blue and bold'.blue.bold);
-console.log('I am black with a white background'.black.bg_white);
-console.log('I am black with a red background'.red.inverse);
+console.log('Colors :');
+console.log('  RESET   : ', 'TEST'.reset);
+console.log('  BLACK   : ', 'TEST'.black);
+console.log('  RED     : ', 'TEST'.red);
+console.log('  GREEN   : ', 'TEST'.green);
+console.log('  YELLOW  : ', 'TEST'.yellow);
+console.log('  BLUE    : ', 'TEST'.blue);
+console.log('  MAGENTA : ', 'TEST'.magenta);
+console.log('  CYAN    : ', 'TEST'.cyan);
+console.log('  WHITE   : ', 'TEST'.white);
+console.log('  GRAY    : ', 'TEST'.gray);
+console.log('  GREY    : ', 'TEST'.grey);
+
+console.log('Bright Colors :');
+console.log('  BLACK   : ', 'TEST'.bright_black);
+console.log('  RED     : ', 'TEST'.bright_red);
+console.log('  GREEN   : ', 'TEST'.bright_green);
+console.log('  YELLOW  : ', 'TEST'.bright_yellow);
+console.log('  BLUE    : ', 'TEST'.bright_blue);
+console.log('  MAGENTA : ', 'TEST'.bright_magenta);
+console.log('  CYAN    : ', 'TEST'.bright_cyan);
+console.log('  WHITE   : ', 'TEST'.bright_white);
+
+console.log('Backgrounds :');
+console.log('  BLACK   : ', 'TEST'.bg_black);
+console.log('  RED     : ', 'TEST'.bg_red);
+console.log('  GREEN   : ', 'TEST'.bg_green);
+console.log('  YELLOW  : ', 'TEST'.bg_yellow);
+console.log('  BLUE    : ', 'TEST'.bg_blue);
+console.log('  MAGENTA : ', 'TEST'.bg_magenta);
+console.log('  CYAN    : ', 'TEST'.bg_cyan);
+console.log('  WHITE   : ', 'TEST'.bg_white);
+
+console.log('Bright Backgrounds :');
+console.log('  BLACK   : ', 'TEST'.bright_bg_black);
+console.log('  RED     : ', 'TEST'.bright_bg_red);
+console.log('  GREEN   : ', 'TEST'.bright_bg_green);
+console.log('  YELLOW  : ', 'TEST'.bright_bg_yellow);
+console.log('  BLUE    : ', 'TEST'.bright_bg_blue);
+console.log('  MAGENTA : ', 'TEST'.bright_bg_magenta);
+console.log('  CYAN    : ', 'TEST'.bright_bg_cyan);
+console.log('  WHITE   : ', 'TEST'.bright_bg_white);
+
+console.log('Styles :');
+console.log('  BOLD          : ', 'TEST'.bold);
+console.log('  FAINT         : ', 'TEST'.faint);
+console.log('  ITALIC        : ', 'TEST'.italic);
+console.log('  UNDERLINE     : ', 'TEST'.underline);
+console.log('  SLOW BLINK    : ', 'TEST'.slow_blink);
+console.log('  RAPID BLINK   : ', 'TEST'.rapid_blink);
+console.log('  INVERSE       : ', 'TEST'.inverse);
+console.log('  HIDDEN        : ', 'TEST'.hidden);
+console.log('  STRIKETHROUGH : ', 'TEST'.strikethrough);
+console.log('  FRAMED        : ', 'TEST'.framed);
+console.log('  ENCIRCLED     : ', 'TEST'.encircled);
+console.log('  OVERLINED     : ', 'TEST'.overlined);
