6fb5a9a78ccf0027c355656321bbc4163927f587
Sean Hamilton <sean@hamistudios.com>
2018-06-20 14:15:39 +0100

    Added new Logger tests

--- tests/Logger.test.js
+++ tests/Logger.test.js
@@ -2,81 +2,233 @@ const
   // modules
   colors    = require('./../src/Colors'),
   Logger    = require('./../src/Logger'),
-  devNull   = require('dev-null')
+  stream    = require('stream')
 ;
 
-describe('Logger.test', () => {
+let
+  logger  = undefined
+;
 
-  let
+describe('generic tests', () => {
+
+  beforeEach(() => {
+
+    let
+      devNull = function () {
+
+        if(!(this instanceof devNull)) return new devNull();
+
+        stream.Writable.call(this);
+
+      }
+    ;
+
+    devNull._write = function(chunk, encoding, callback) {
+
+      callback();
+
+    };
+
     logger    = new Logger({
       streams: {
-        verbose   : devNull(),
+        verbose   : () => {},
         info      : devNull(),
-        error     : devNull(),
-        warning   : devNull(),
-        notice    : devNull(),
-        debug     : devNull(),
-        log       : devNull()
-      },
-      formats: {
-        date   : function () {
-
-          return 'date';
-
-        }
+        error     : () => {},
+        warning   : 'invalid',
+        notice    : () => {},
+        debug     : () => {},
+        log       : () => {}
       },
       eol    : '',
       verbose: true,
       debug  : true
-    })
-  ;
-
-  // verbose
-  test('#verbose()', () => {
-
-    expect(colors.strip(logger.verbose('testing'))).toBe('[date] verbose: testing');
+    });
 
   });
 
-  // info
-  test('#info()', () => {
+  test('logging should return the formatted message', () => {
 
-    expect(colors.strip(logger.info('testing'))).toBe('[date] info: testing');
-
-  });
-
-  // error
-  test('#error()', () => {
-
-    expect(colors.strip(logger.error('testing'))).toBe('[date] error: testing');
+    expect(logger.log('test')).toBeTruthy(); // can't check the message since {date} should be anything
 
   });
 
-  // warning
-  test('#warning()', () => {
+  test('when logging to a stream it should write the formatted message to the stream', () => {
 
-    expect(colors.strip(logger.warning('testing'))).toBe('[date] warning: testing');
-
-  });
-
-  // notice
-  test('#notice()', () => {
-
-    expect(colors.strip(logger.notice('testing'))).toBe('[date] notice: testing');
+    expect(logger.info('test')).toBeTruthy();
 
   });
 
-  // debug
-  test('#debug()', () => {
+  test('when the specified stream is not a Writable stream or a function it should error', () => {
 
-    expect(colors.strip(logger.debug('testing'))).toBe('[date] debug: testing');
+    expect(logger.warning('test')).toBeInstanceOf(Error);
 
   });
 
-  // log
-  test('#log()', () => {
+  test('test if formats which are not functions just set the value instead', () => {
 
-    expect(colors.strip(logger.log('testing'))).toBe('[date] log: testing');
+    logger.setOptions({
+      format: '[{{date}}] {{type}}: {{message}} {{nonFunction}}',
+      formats: {
+        date: 'date',
+        nonFunction: 'notAFunc'
+      }
+    });
+
+    expect(logger.log('test')).toBe('[date]  log : test notAFunc');
+
+  });
+
+  test('.verbose() logs when `verbose` is true', () => {
+
+    logger.options.verbose = true;
+
+    expect(logger.verbose('test')).toBeTruthy();
+
+  });
+
+  test('.debug() logs when `debug` is true', () => {
+
+    logger.options.debug = true;
+
+    expect(logger.debug('test')).toBeTruthy();
+
+  });
+
+  test('.verbose() does not logs when `verbose` is false', () => {
+
+    logger.options.verbose = false;
+
+    expect(logger.verbose('test')).toBeFalsy();
+
+  });
+
+  test('.debug() does not logs when `debug` is false', () => {
+
+    logger.options.debug = false;
+
+    expect(logger.debug('test')).toBeFalsy();
+
+  });
+
+});
+
+describe('option methods', () => {
+
+  beforeAll(() => {
+
+    logger    = new Logger({
+      streams: {
+        verbose   : () => {},
+        info      : () => {},
+        error     : () => {},
+        warning   : () => {},
+        notice    : () => {},
+        debug     : () => {},
+        log       : () => {}
+      },
+      eol    : '',
+      verbose: true,
+      debug  : true
+    });
+
+  });
+
+  test('.setOptions() should modify the Logger options', () => {
+
+    logger.setOptions({
+      eol: '>'
+    });
+
+    expect(logger.options.eol).toBe('>');
+
+  });
+
+  test('.setDebug() should set whether the logger should output debug logs', () => {
+
+    logger.setDebug(false);
+
+    expect(logger.options.debug)
+      .toBe(false);
+
+  });
+
+  test('.setVerbose() should set whether the logger should output verbose logs', () => {
+
+    logger.setVerbose(false);
+
+    expect(logger.options.verbose)
+      .toBe(false);
+
+  });
+
+});
+
+describe('log methods', () => {
+
+  beforeEach(() => {
+
+    logger    = new Logger({
+      streams: {
+        verbose   : () => {},
+        info      : () => {},
+        error     : () => {},
+        warning   : () => {},
+        notice    : () => {},
+        debug     : () => {},
+        log       : () => {}
+      },
+      eol    : '',
+      verbose: true,
+      debug  : true
+    });
+
+    logger.options.formats.date = jest.fn(function () {
+
+      return 'date';
+
+    });
+
+  });
+
+  test('.verbose() should format the arguments into a verbose log', () => {
+
+    expect(colors.strip(logger.verbose('testing'))).toBe('[date]  verbose : testing');
+
+  });
+
+  test('.info() should format the arguments into a info log', () => {
+
+    expect(colors.strip(logger.info('testing'))).toBe('[date]  info : testing');
+
+  });
+
+  test('.error() should format the arguments into a error log', () => {
+
+    expect(colors.strip(logger.error('testing'))).toBe('[date]  error : testing');
+
+  });
+
+  test('.warning() should format the arguments into a warning log', () => {
+
+    expect(colors.strip(logger.warning('testing'))).toBe('[date]  warning : testing');
+
+  });
+
+  test('.notice() should format the arguments into a notice log', () => {
+
+    expect(colors.strip(logger.notice('testing'))).toBe('[date]  notice : testing');
+
+  });
+
+  test('.debug() should format the arguments into a debug log', () => {
+
+    expect(colors.strip(logger.debug('testing'))).toBe('[date]  debug : testing');
+
+  });
+
+  test('.log() should format the arguments into a log log', () => {
+
+    expect(colors.strip(logger.log('testing'))).toBe('[date]  log : testing');
 
   });
 
