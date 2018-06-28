1d8cc48bca5c72b584049ef5ea4e5d4f25641e21
Sean Hamilton <sean@hamistudios.com>
2018-06-28 12:55:24 +0100

    Updated example to use chaining

--- examples/example.js
+++ examples/example.js
@@ -4,4 +4,9 @@ const
   logger      = new Logger()
 ;
 
-logger.log('Hello World'.red.underline, { 'can log': true });
+logger
+  .log('Hello World'.red.underline, { 'can log': true })
+  .clear()
+  .blank()
+  .log('Hello World'.bright_blue.inverse, { 'can log': true })
+  .info('Hello World'.bright_yellow);
